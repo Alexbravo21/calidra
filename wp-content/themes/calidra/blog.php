cc792e2f6cc9982eb6c25b7dfafe4b3b0489b699
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-06-11 21:03:00 -0500

    Fixed home usos-cotidianos title position and aligned all blog entries to be the same height

--- wp-content/themes/calidra/blog.php
+++ wp-content/themes/calidra/blog.php
@@ -20,18 +20,20 @@
         <div class="columns is-multiline">
             <?php if($blog_query->have_posts()) : ?>
                 <?php while($blog_query->have_posts()) : $blog_query->the_post(); ?>
-                    <div class="column is-one-third">
+                    <div class="column is-one-third is-flex">
                         <a href="<?=get_post_permalink(); ?>" style="display:block;" class="reliable-old-lady<?php $reliable = array_rand($reliableArray); echo $reliableArray[$reliable]; ?>">
-                            <div class="blog-section-mini-text">
-                                <h3><?=get_the_title(); ?></h3>
-                                <p class="blog-section-mini-text-desc"><?=get_field('extracto_blog'); ?></p>
-                                <p class="blog-section-mini-text-meta">
-                                    <span><?php print_r(get_the_category()[0]->name); ?></span>
-                                    <?=get_post_time('d-m-Y'); ?>
-                                </p>
-                            </div>
-                            <div class="blog-section-mini-img">
-                                <img src="<?=the_post_thumbnail_url(); ?>" alt="">
+                            <div class="blog-section-mini-cont">
+                                <div class="blog-section-mini-cont-text">
+                                    <h3><?=get_the_title(); ?></h3>
+                                    <p class="blog-section-mini-cont-text-desc"><?=get_field('extracto_blog'); ?></p>
+                                    <p class="blog-section-mini-cont-text-meta">
+                                        <!-- <span><?php // print_r(get_the_category()[0]->name); ?></span> -->
+                                        <?=get_post_time('d-m-Y'); ?>
+                                    </p>
+                                </div>
+                                <div class="blog-section-mini-cont-img">
+                                    <img src="<?=the_post_thumbnail_url(); ?>" alt="">
+                                </div>
                             </div>
                         </a>
                     </div>
