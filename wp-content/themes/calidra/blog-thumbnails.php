0e63568e91f13fc6cfd6861bae8cbb362ce80e79
Alejandro Bravo <alejandrob@spacebarmedia.com>
2021-06-11 21:30:24 -0500

    Fixed blog mini slider title alignment and added link tag to their own post

--- wp-content/themes/calidra/blog-thumbnails.php
+++ wp-content/themes/calidra/blog-thumbnails.php
@@ -2,7 +2,7 @@
     $posts = get_posts();
 ?>
 <section class="blog-thumbnails section">
-    <h2>Blog</h2>
+    <h2 class="container">Blog</h2>
     <div class="slider-navigation-previous-calidra"><div class="flecha"></div></div>
     <div class="slider-navigation-next-calidra"><div class="flecha"></div></div>
     <div class="blog-thumbnails-contenedor">
@@ -10,8 +10,10 @@
         <div id="carousel-demo" class="carousel">
             <?php $cont = 1; foreach($posts as $post): ?>
                 <div class="item-<?=$cont ?> car-item">
-                    <img src="<?=get_the_post_thumbnail_url($post->ID)?>" alt="">
-                    <p><?=$post->post_title ?></p>
+                    <a href="<?=$post->guid ?>">
+                        <img src="<?=get_the_post_thumbnail_url($post->ID)?>" alt="">
+                        <p><?=$post->post_title ?></p>
+                    </a>
                 </div>
             <?php $cont = $cont + 1; endforeach; ?>
         </div>
